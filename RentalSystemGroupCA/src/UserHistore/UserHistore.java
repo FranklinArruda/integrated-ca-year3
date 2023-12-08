1747232f1ddd47722b3b67be34899147d4f0e652
Enio Kelsio <100359737+EnioKelsio@users.noreply.github.com>
2023-12-08 10:10:11 +0000

    Update UserHistore.java

--- RentalSystemGroupCA/src/UserHistore/UserHistore.java
+++ RentalSystemGroupCA/src/UserHistore/UserHistore.java
@@ -1,9 +1,10 @@
-package UserHistore;
-
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+
+package UserHistore;
+
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashMap;
