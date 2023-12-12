7cbfaf61f246ed4553eb88a7e5c65c1edf7402f3
Bigslim <2021336@student.cct.ie>
2023-12-12 16:02:33 +0000

    Create Catalog.java

---
+++ RentalSystemGroupCA/src/Rental/Catalog.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Rental;
+
+import Film.Film;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author diego
+ */
+public class Catalog {
+    private List<Film> films;
+
+    public Catalog() {
+        this.films = new ArrayList<>();
+    }
+    
+    public void addFilm(Film film){
+        films.add(film);
+    }
+    
+    public List<Film> getFilms(){
+        return films; 
+    }
+    
+    
+    
+}
